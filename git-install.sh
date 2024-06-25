5e9628355bc7c004195629144d19e35c3d04c5b5
Andika Candra Pristiawan <45266278+awankumay@users.noreply.github.com>
2024-06-25 14:26:44 +0700

    Delete git-install.sh

--- git-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Get Repository Git-Core"
-sudo add-apt-repository ppa:git-core/ppa
-echo "Install / Upgrade Git"
-sudo apt-get install git -y
-echo "Install / Upgrade Git Done!"
-git --version
