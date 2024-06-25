17f41ca38a5ed639b90f28b96177c6396a9877c3
Andika Candra Pristiawan <45266278+awankumay@users.noreply.github.com>
2024-06-25 13:34:44 +0700

    Update wazuh-agent.sh

--- wazuh-agent.sh
+++ wazuh-agent.sh
@@ -4,7 +4,7 @@ echo "Get Source Wazuh Agent & Install....."
 wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.0-1_amd64.deb && sudo WAZUH_MANAGER='10.245.245.42' WAZUH_AGENT_GROUP='OwlexaHealthcare' WAZUH_AGENT_NAME=$(hostname) dpkg -i ./wazuh-agent_4.8.0-1_amd64.deb
 echo "Installation Wazuh Agent Done!"
 echo "Get Restart Service Wazuh Agent & Enable Service"
-sudo systemctl daemon-reload && sudo systemctl enable wazuh-agent && sudo systemctl start wazuh-agent
+systemctl daemon-reload && sudo systemctl enable wazuh-agent && systemctl start wazuh-agent
 echo "Proses Service Done!"
 echo "Get Disable GLPI Agent Inventory...."
 systemctl stop glpi-agent.service && systemctl disable glpi-agent.service
