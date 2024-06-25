eedd610929b57340b536d2ccbe649ae9f4791976
Andika Candra Pristiawan <45266278+awankumay@users.noreply.github.com>
2024-06-25 14:27:59 +0700

    Update git-install.sh

--- git-install.sh
+++ git-install.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 
-echo "Get Repository Git-Core"
-sudo add-apt-repository ppa:git-core/ppa
-echo "Install / Upgrade Git"
-sudo apt-get install git -y
-echo "Install / Upgrade Git Done!"
-git --version
